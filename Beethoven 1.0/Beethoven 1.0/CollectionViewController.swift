b3bc5dfe5cb836321b6c0b1ae8c688de8fb3b020
shigapovaa <shigapova.alsu@gmail.com>
2017-07-12 15:19:08 +0300

    Примерный дизайн главного экрана и добавление нескольких экранов для языков жестов

---
+++ Beethoven 1.0/Beethoven 1.0/CollectionViewController.swift
@@ -0,0 +1,66 @@
+//
+//  CollectionViewController.swift
+//  Beethoven 1.0
+//
+//  Created by Alina on 12.07.17.
+//  Copyright © 2017 Alina. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 33
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        imageView.image = UIImage(named:"1")
+        cell.backgroundView = imageView
+        
+        // Configure the cell
+    
+        return cell
+    }
+
+
+
+}
